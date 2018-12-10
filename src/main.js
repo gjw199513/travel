15506b67469a59b8fc4089bcfdd6d5a8cb2de350
gjw199513@163.com <gjw605134015>
2018-12-10 22:04:52 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -3,13 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 页面初始化
+import './assets/style/reset.css'
+// 移动端1像素边框解决方案
+import './assets/style/border.css'
+// 移动端click事件300毫秒延迟问题
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
 /* eslint-disable no-new */
+
 new Vue({
   el: '#app',
+  /* 在es6中键和值相同可以省略值 */
   router,
   components: { App },
   template: '<App/>'
 })
+
+// 路由就是根据网址的不同，返回不同的内容给用户
