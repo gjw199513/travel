aeca49c26e66b51dbf69eb316dc169ec91ab3775
gjw199513 <gjw199513@163.com>
2018-12-11 17:55:26 +0800

    图标区域

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,19 +2,22 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
 
 export default {
   name: 'Home',
   components: {
     // 将驼峰形式自动转换为横线形式
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
